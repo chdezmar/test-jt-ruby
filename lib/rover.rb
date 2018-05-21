8d36060095845bcbfcd2c428db39553bd8c1678e
Carlos <chdezmar@gmail.com>
2018-05-21 20:36:58 +0200

    Add #turn_right and #turn_left

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,5 +1,7 @@
 class Rover
 
+  ORIENTATIONS = [:N, :E, :S, :W]
+
   ORIENTATION_MAP = {
     N: [0,1],
     E: [1,0],
@@ -25,6 +27,14 @@ class Rover
 
   def move_backwards
     @position = [position, ORIENTATION_MAP[orientation]].transpose.map {|x| x.reduce(:-)}
+  end
+
+  def turn_right
+    @orientation = ORIENTATIONS[(ORIENTATIONS.index(orientation) + 1) % ORIENTATIONS.size]
+  end
+
+  def turn_left
+    @orientation = ORIENTATIONS[(ORIENTATIONS.index(orientation) - 1) % ORIENTATIONS.size]
   end
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -4,7 +4,7 @@ describe Rover do
 
   subject(:rover) { described_class.new }
 
-  context '#at initialization' do
+  context 'at initialization' do
     it 'has no position' do
       expect(rover.position).to be_an_instance_of(Array)
       expect(rover.position).to be_empty
@@ -14,7 +14,7 @@ describe Rover do
     end
   end
 
-  context '#deploy' do
+  describe '#deploy' do
     it 'can be deployed by passing a starting point and orientation' do
       rover.deploy([0,0], :N)
       expect(rover.position).to eq([0,0])
@@ -22,49 +22,99 @@ describe Rover do
     end
   end
 
-  context '#move_forward' do
-    it 'moves forward when orientation is N' do
-      rover.deploy([2,2], :N)
-      rover.move_forward
-      expect(rover.position).to eq([2,3])
+  context 'Moving' do
+    describe '#move_forward' do
+      it 'moves forward when orientation is N' do
+        rover.deploy([2,2], :N)
+        rover.move_forward
+        expect(rover.position).to eq([2,3])
+      end
+      it 'moves forward when orientation is E' do
+        rover.deploy([2,2], :E)
+        rover.move_forward
+        expect(rover.position).to eq([3,2])
+      end
+      it 'moves forward when orientation is S' do
+        rover.deploy([2,2], :S)
+        rover.move_forward
+        expect(rover.position).to eq([2,1])
+      end
+      it 'moves forward when orientation is W' do
+        rover.deploy([2,2], :W)
+        rover.move_forward
+        expect(rover.position).to eq([1,2])
+      end
     end
-    it 'moves forward when orientation is E' do
-      rover.deploy([2,2], :E)
-      rover.move_forward
-      expect(rover.position).to eq([3,2])
-    end
-    it 'moves forward when orientation is S' do
-      rover.deploy([2,2], :S)
-      rover.move_forward
-      expect(rover.position).to eq([2,1])
-    end
-    it 'moves forward when orientation is W' do
-      rover.deploy([2,2], :W)
-      rover.move_forward
-      expect(rover.position).to eq([1,2])
+
+    describe '#move_backwards' do
+      it 'moves backwards when orientation is N' do
+        rover.deploy([2,2], :N)
+        rover.move_backwards
+        expect(rover.position).to eq([2,1])
+      end
+      it 'moves backwards when orientation is E' do
+        rover.deploy([2,2], :E)
+        rover.move_backwards
+        expect(rover.position).to eq([1,2])
+      end
+      it 'moves backwards when orientation is S' do
+        rover.deploy([2,2], :S)
+        rover.move_backwards
+        expect(rover.position).to eq([2,3])
+      end
+      it 'moves backwards when orientation is W' do
+        rover.deploy([2,2], :W)
+        rover.move_backwards
+        expect(rover.position).to eq([3,2])
+      end
     end
   end
 
-  context '#move_backwards' do
-    it 'moves backwards when orientation is N' do
-      rover.deploy([2,2], :N)
-      rover.move_backwards
-      expect(rover.position).to eq([2,1])
+  context 'Turning' do
+    describe '#turn_right' do
+      it 'turns right when orientation is N' do
+        rover.deploy([2,2], :N)
+        rover.turn_right
+        expect(rover.orientation).to eq(:E)
+      end
+      it 'turns right when orientation is E' do
+        rover.deploy([2,2], :E)
+        2.times {rover.turn_right}
+        expect(rover.orientation).to eq(:W)
+      end
+      it 'turns right when orientation is S' do
+        rover.deploy([2,2], :S)
+        3.times {rover.turn_right}
+        expect(rover.orientation).to eq(:E)
+      end
+      it 'turns right when orientation is W' do
+        rover.deploy([2,2], :W)
+        rover.turn_right
+        expect(rover.orientation).to eq(:N)
+      end
     end
-    it 'moves backwards when orientation is E' do
-      rover.deploy([2,2], :E)
-      rover.move_backwards
-      expect(rover.position).to eq([1,2])
-    end
-    it 'moves backwards when orientation is S' do
-      rover.deploy([2,2], :S)
-      rover.move_backwards
-      expect(rover.position).to eq([2,3])
-    end
-    it 'moves backwards when orientation is W' do
-      rover.deploy([2,2], :W)
-      rover.move_backwards
-      expect(rover.position).to eq([3,2])
+
+    describe '#turn_left' do
+      it 'turns left when orientation is N' do
+        rover.deploy([2,2], :N)
+        rover.turn_left
+        expect(rover.orientation).to eq(:W)
+      end
+      it 'turns left when orientation is E' do
+        rover.deploy([2,2], :E)
+        2.times {rover.turn_left}
+        expect(rover.orientation).to eq(:W)
+      end
+      it 'turns left when orientation is S' do
+        rover.deploy([2,2], :S)
+        3.times {rover.turn_left}
+        expect(rover.orientation).to eq(:W)
+      end
+      it 'turns left when orientation is W' do
+        rover.deploy([2,2], :W)
+        rover.turn_left
+        expect(rover.orientation).to eq(:S)
+      end
     end
   end
 end
